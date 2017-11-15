dc118825ca07f0e47896281eec56bfa0780109ae
zhyucan <zhyucan@gmail.com>
2017-11-15 15:28:17 +0800

    uptade ch8

--- ch8/08.scm
+++ ch8/08.scm
@@ -208,12 +208,42 @@ l is (equal? eq? eqan? eqlist? eqpair?)
 ; The Ninth Commandment
 ; Abstract common patterns with a new function
 
+nexp is (+ (× 3 6) (↑ 8 2))
+(value nexp) is (+ 18 64) is 82
+
+(define value
+	(lambda (nexp)
+		(cond
+			((atom? nexp) nexp)
+			((eq? (car nexp) (quote +))
+				(+ (value (car (cdr nexp))) 
+					(value (car (cdr (cdr nexp))))))
+			((eq? (car nexp) (quote ×))
+				(× (value (car (cdr nexp))) 
+					(value (car (cdr (cdr nexp))))))
+			(else
+				(↑ (value (car (cdr nexp))) 
+					(value (car (cdr (cdr nexp)))))))))
+
+(define 1st-sub-exp
+	(lambda (aexp)
+		(car (cdr aexp))))
+
+(define 2nd-sub-exp
+	(lambda (aexp)
+		(car (cdr (cdr aexp)))))
+
+(define operator
+	(lambda (aexp)
+		(car aexp)))
+
+value with help functions
 (define value
 	(lambda (nexp)
 		(cond
 			((atom? nexp) nexp)
 			((eq? (operator nexp) (quote +))
-				(加 (value (1st-sub-exp nexp))
+				(+ (value (1st-sub-exp nexp))
 					(value (2nd-sub-exp nexp))))
 			((eq? (operator nexp) (quote ×))
 				(× (value (1st-sub-exp nexp)) 
@@ -222,12 +252,41 @@ l is (equal? eq? eqan? eqlist? eqpair?)
 				(↑ (value (1st-sub-exp nexp)) 
 					(value (2nd-sub-exp nexp)))))))
 
+the 3 answers are same except for the +, ×, ↑
+
 (define atom-to-function
 	(lambda (x)
 		(cond
-			((eq? x (quote 加)) +)
+			((eq? x (quote +)) +)
 			((eq? x (quote ×)) ×)
 			(else ↑))))
+
+atom-to-function is a function
+takes one argument x, x is an atom
+return the function +, if (eq? x (quote +))
+return the function ×, if (eq? x (quote ×))
+return the function ↑, otherwise
+
+nexp is (+ 5 3)
+(atom-to-function (operator nexp)) is the function +
+not the atom +
+
+rewrite value
+(define value
+	(lambda (nexp)
+		(cond
+			((atom? nexp) nexp)
+			(else
+				((atom-to-function (operator nexp)) 
+					(value (1st-sub-exp nexp))
+					(value (2nd-sub-exp nexp)))))))
+
+time for an apple
+one a day kees the doctor away
+
+a is cup
+lat is (coffee cup tea cup and hick cup)
+(multirember a lat) is (coffee tea and hick)
 
 (define multirember
   (lambda (a lat)
@@ -238,21 +297,157 @@ l is (equal? eq? eqan? eqlist? eqpair?)
      (else (cons (car lat)
             (multirember a (cdr lat)))))))
 
+write multirember-f
 (define multirember-f
 	(lambda test?
 	  (lambda (a lat)
 	    (cond
 	     ((null? lat) (quote ()))
 	     ((test? (car lat) a)
-	       ((multirember-f test?) a (cdr lat)))
+	      ((multirember-f test?) a (cdr lat)))
 	     (else (cons (car lat)
-	             ((multirember-f test?) a (cdr lat))))))))
+	            ((multirember-f test?) a (cdr lat)))))))
 
-test? is eq?
 a is tuna
+test? is eq?
 lat is (shrimp salad tuna salad and tuna)
-((multirember-f test?) a lat) is (shrimp salad salad and)
+((multirember-f eq?) a lat) is (shrimp salad salad and)
 
 define multirember-eq? using multirember-f
 (define multirember-eq?
 	(multirember-f test?))
+
+when multirember-f looks for tuna in lat
+test? always stands for eq?
+a always stands for tuna
+
+we can combine a and test?
+test? can be a function
+it takes one argument
+compare the argument to tuna
+
+onw way to write this function
+(define eq?-tuna
+	(eq?-c k))
+
+a different way
+(define eq?-tuna
+	(eq?-c (quote tuna)))
+
+write multiremberT
+multirember-f takes test? and returns a function
+multiremberT takes a function like eq?-tuna and a lat
+
+(define multiremberT
+  (lambda (test? lat)
+    (cond
+     ((null? lat) (quote ()))
+     ((test? (car lat))
+      (multiremberT test? (cdr lat)))
+     (else (cons (car lat)
+            (multiremberT test? (cdr lat)))))))
+
+test? is eq?-tuna
+lat is (shrimp salad tuna salad and tuna)
+(multiremberT test? lat) is (shrimp salad salad and)
+
+(define multirember&co
+	(lambda (a lat col)
+		(cond
+			((null? lat) (col (quote ()) (quote ())))
+			((eq? (car lat) a) 
+				(multirember&co a (cdr lat)
+					(lambda (newlat seen)
+						(col newlat (cons (car lat) seen)))))
+			(else
+				(multirember&co a (cdr lat)
+					(lambda (newlat seen)
+						(col (cons (car lat) newlat) seen)))))))
+
+write a-friend
+it takes 2 arguments
+it asks whether the second one is the empty list
+it ignore its first argument
+(define a-friend
+	(lambda (x y)
+		(null? y)))
+
+a is tuna
+lat is (strawberries tuna and swordfish)
+col is a-friend
+(multirember&co a lat col) is ... not simple
+
+a is tuna
+lat is ()
+col is a-friend
+(multirember&co a lat col) is #t
+a-friend uses in the first answer
+a-friend makes sure that its second argument is empty
+
+a is tuna
+lat is (tuna)
+col is a-friend
+(multirember&co a lat col) is ?
+
+multirember&co asks (eq? (car lat) (quote tuna))
+it recurs on ()
+
+the natural recursion is 
+(multirember&co a (cdr lat)
+	(lambda (newlat seen)
+		(col newlat (cons (car lat) seen))))
+
+multirember&co takes 3 arguments
+the first one is tuna
+the second one is ()
+the third one is a new function
+
+the name of the third argument is col
+
+col stands for "collector"
+a collector is called a "continuation"
+
+a new collector
+(define new-friend
+	(lambda (newlat seen)
+		(col newlat (cons (car lat) seen))))
+
+if (car lat) is tuna
+col is a-friend
+the new collector by a new way
+(define new-friend
+	(lambda (newlat seen)
+		(col newlat (cons (quote tuna) seen))))
+
+we can replace col with a-friend
+(define new-friend
+	(lambda (newlat seen)
+		(a-friend newlat (cons (quote tuna) seen))))
+
+a is tuna
+lat is (and tuna)
+(multirember&co a lat a-friend) is ?
+
+multirember&co asks else
+it recurs on 
+(multirember&co a (cdr lat)
+	(lambda (newlat seen)
+		(col (cons (car lat) newlat) seen)))
+
+(define latest-friend
+	(lambda (newlat seen)
+		(a-friend (cons (quote and) newlat) seen)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
